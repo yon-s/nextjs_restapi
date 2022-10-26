82e6ad85dab96717c1a37505a230d3328677d329
yon <shuichi.yonehara@gmail.com>
2022-10-26 17:59:55 +0900

    change code

--- rest_api/wsgi.py
+++ rest_api/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
-from django.core.wsgi import
 
+from dj_static import Cling
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api.settings')
 
